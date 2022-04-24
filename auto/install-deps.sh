f013180eb558498d444a04ba24a3b3474884e9e2
Danny Hadley <dadleyy@gmail.com>
2022-04-24 14:06:26 -0400

    cleanup of original code

--- auto/install-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z `which arduino-cli` ]]; then
-  echo "Missing arduino-cli"
-  exit 1
-fi
-
-arduino-cli lib uninstall DFRobot_GDL
-arduino-cli lib install --git-url https://github.com/dadleyy/DFRobot_GDL.git
