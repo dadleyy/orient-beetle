800b1519ed77ab591dcf45a37169eafcf239f357
Danny Hadley <dadleyy@gmail.com>
2022-07-16 14:13:51 -0400

    commenting things

--- tools/beetle-updater/src/main.rs
+++ tools/beetle-updater/src/main.rs
@@ -78,6 +78,9 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
     log::warn!("{run_id} force initial run");
   }
 
+  // Attempt to parse the contents of the `semver_storage` configuration option as a valid sematic
+  // version string. We will be using this to compare with the cloud value, and updating it to any
+  // installed version on successful download.
   let version_data = async_std::fs::read(&config.semver_storage)
     .await
     .map_err(|error| log::warn!("{error}"))
@@ -89,11 +92,12 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
 
   log::debug!("current version - {current_version:?}");
 
-  let normalized = config.repo.trim_end_matches("https://github.com/");
+  let normalized = config.repo.trim_start_matches("https://github.com/");
   let url = format!("https://api.github.com/repos/{normalized}");
 
-  log::debug!("running github release update check for {config:?} (@ {url})");
+  log::debug!("running github release update check (@ {url})");
 
+  // Use the `/releases/latest` api route to fetch whatever release was latest.
   let mut response = surf::get(format!("{}/releases/latest", url))
     .header("Authorization", format!("token {auth_token}"))
     .await
@@ -121,6 +125,8 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
     latest.assets.len()
   );
 
+  // Attempt to parse the github response payload's `name` field as a semantic version. This
+  // ultimately means that we require all release names to be a semantic verison.
   let should_update = match (
     flags,
     current_version,
@@ -128,8 +134,13 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
   ) {
     (InitialRunFlags::Update, _, _) => true,
 
+    (_, None, Some(next)) => {
+      log::warn!("no current version found, assuming valid update to {next}");
+      true
+    }
+
     (_, Some(current), Some(next)) if next > current => {
-      log::info!("new version found");
+      log::info!("new version found ({current} -> {next})");
       true
     }
 
@@ -150,18 +161,6 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
 
   log::info!("proceeding with update, checking destination");
 
-  match async_std::fs::metadata(&config.extraction.destination).await {
-    Ok(meta) if meta.file_type().is_dir() => {
-      let backup = format!("{}.{}.bak", &config.extraction.destination, run_id);
-      log::warn!("{} exists, backing up to {}", config.extraction.destination, backup);
-      async_std::fs::rename(&config.extraction.destination, &backup).await?;
-    }
-    Err(error) if error.kind() == ErrorKind::NotFound => {
-      log::info!("clean destination, moving on");
-    }
-    unknown => log::warn!("unknown destination check - {unknown:?}"),
-  }
-
   let asset = match latest.assets.into_iter().find(|item| {
     let match_prefix = item.name.starts_with(&config.artifact_naming.starts_with);
     let match_suffix = item.name.ends_with(&config.artifact_naming.ends_with);
@@ -173,6 +172,8 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
 
   log::info!("found artifact match {} ('{}' @ '{}')", asset.id, asset.name, asset.url);
 
+  // We're assuming an invariant here that the github api for assets will always attempt to return
+  // a redirect when requesting the data for a specific asset.
   let locate_response = surf::get(&asset.url)
     .header("Accept", "application/octet-stream")
     .header("Authorization", format!("token {}", auth_token))
@@ -195,7 +196,7 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
     .map(|value| value.as_str().to_string())
     .ok_or_else(|| Error::new(ErrorKind::Other, "bad response from github"))?;
 
-  log::info!("real location: {real_location}");
+  log::info!("found real location, downloading.");
 
   let mut download_response = surf::get(&real_location)
     .await
@@ -211,14 +212,17 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
 
   log::info!("download headers received, receiving bytes");
 
+  // The future awaited up to this point only implies that the header section of our http request
+  // has been received. To actually load the contents of the asset file, we need to read the rest
+  // of the request into memory.
   let bytes = download_response.body_bytes().await.map_err(|error| {
     log::warn!("{error}");
     Error::new(ErrorKind::Other, "failed-download")
   })?;
 
+  // Prepare a temporary directory that our tarball will be unpacked into.
   let temp_dir = std::env::temp_dir().join("beetle-updater").join(&run_id.to_string());
   log::info!("download complete, creating temp dir {:?}", temp_dir);
-
   async_std::fs::create_dir_all(&temp_dir).await?;
 
   let decompressor = flate2::read::GzDecoder::new(bytes.as_slice());
@@ -236,8 +240,30 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
     entry.unpack(&entry_destination)?;
   }
 
-  async_std::fs::rename(&temp_dir, &config.extraction.destination).await?;
+  // With our `tmp` directory now succesfully full of good things, we should make sure to prepare
+  // our destination. The only two cases we're dealing with currently are:
+  //
+  // 1. the destination existing -> we rename.
+  // 2. the destination not existing -> we're happy.
+  match async_std::fs::metadata(&config.extraction.destination).await {
+    Ok(meta) if meta.file_type().is_dir() => {
+      let backup = format!("{}.{}.bak", &config.extraction.destination, run_id);
+      log::warn!("{} exists, backing up to {}", config.extraction.destination, backup);
+      async_std::fs::rename(&config.extraction.destination, &backup).await?;
+    }
+    Err(error) if error.kind() == ErrorKind::NotFound => {
+      log::info!("clean destination, moving on");
+    }
+    unknown => log::warn!("unknown destination check - {unknown:?}"),
+  }
 
+  // Add the version immedaitely after the configured destination to get the final output directory
+  // that we will move our artifact into from the `tmp` directory.
+  let full_destination_path = format!("{}/{}", config.extraction.destination, latest.name);
+  async_std::fs::create_dir_all(&full_destination_path).await?;
+  async_std::fs::rename(&temp_dir, &full_destination_path).await?;
+
+  // Update the version storage file with our new release name.
   log::info!("success, writing new version to storage");
   let mut file = async_std::fs::File::create(&config.semver_storage).await?;
   async_std::write!(&mut file, "{}", latest.name).await?;
@@ -248,7 +274,7 @@ async fn github_release(config: &GithubUpdaterConfig, flags: &InitialRunFlags) -
 async fn run(mut config: UpdaterConfig, mut flags: InitialRunFlags) -> Result<()> {
   let mut interval = async_std::stream::interval(std::time::Duration::from_secs(1));
 
-  log::info!("entering working loop for config: {config:?}");
+  log::info!("entering working loop for config: {config:#?}");
 
   loop {
     interval.next().await;
